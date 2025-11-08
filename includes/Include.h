1c4b4ce64c9bc74a96435c5125e2acaf21367b5e
Yesteb <estebansanchez474@outlook.com>
2025-11-08 08:36:12 -0500

    Case ImprimirArchivos agregado y carperta resources para los menus

---
+++ includes/Include.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+class ImprimirArchivo{
+    void imprimir(char *, int);
+};
+
+#include "Include.hpp"
---
+++ includes/Include.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "Include.h"
+
+void ImprimirArchivo::imprimir(char *rutaArchivo, int charMax) {
+    FILE *fptr = fopen(rutaArchivo, "r");
+    if (fptr == nullptr) {
+        std::cout << "No se ha encontrado el archivo!\n";
+        return;
+    }
+
+    char* buffer = new char[charMax];
+
+    size_t bytesRead;
+    while ((bytesRead = fread(buffer, 1, charMax - 1, fptr)) > 0) {
+        buffer[bytesRead] = '\0';
+        std::cout << buffer;
+    }
+
+    delete[] buffer;
+    fclose(fptr);
+}
