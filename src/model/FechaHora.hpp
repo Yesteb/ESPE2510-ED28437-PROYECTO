5b1619c192b966fd3b881932f53f17c39d1dd234
Julio Sánchez <106382109+Yesteb@users.noreply.github.com>
2025-11-10 12:09:41 -0500

    Update FechaHora.hpp

--- src/model/FechaHora.hpp
+++ src/model/FechaHora.hpp
@@ -19,18 +19,3 @@ void FechaHora::imprimir() const {
     std::cout << fecha << std::endl;
 }
 
-bool FechaHora::esMenorEdad(int diaNacimiento, int mesNacimiento, int anioNacimiento) const {
-    time_t tiempoActual = time(nullptr);
-    struct tm *fechaActual = localtime(&tiempoActual);
-
-    int diaActual = fechaActual->tm_mday;
-    int mesActual = fechaActual->tm_mon + 1;
-    int anioActual = fechaActual->tm_year + 1900;
-
-    int edad = anioActual - anioNacimiento;
-    if(mesActual < mesNacimiento || (mesActual == mesNacimiento && diaActual < diaNacimiento)) edad--;
-    if(edad <= 2) std::cout "La edad es menor o igual a 24 meses de edad" return false;
-    if(edad >= 2) std::cout "La edad es mayor o igual a 65 años de edad" return false;
-    return edad =<18;
-}
-
