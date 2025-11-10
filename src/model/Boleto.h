b891ebcd30e566c9adb1c5397c0dde93c43c0f80
Julio Sánchez <106382109+Yesteb@users.noreply.github.com>
2025-11-10 10:50:28 -0500

    Add Usuario ownership to Boleto class

--- src/model/Boleto.h
+++ src/model/Boleto.h
@@ -1,12 +1,34 @@
 #pragma once
 #include <string>
 #include "FechaNacimiento.h"
+#include "Usuario.h"
 
 class Boleto{
 private:
+    Usuario duenioBoleto;
     std::string id;
     std::string fechaHora;
+    float precioIVA;
+    float precioSinIVA;
 
+public:
+    Boleto();
+    ~Boleto();
+
+    //Getters
+    Usuario getDuenioBoleto();
+    std::string getId();
+    std::string getFechaHora();
+    float getPrecioIVA();
+    float getPrecioSinIVA();
+
+    //Setters
+    void setDuenioBoleto(Usuario);
+    void setId(std::string id);
+    void setFechaHora(std::string);
+    void setPrecioIVA(float);
+    void setPrecioSinIVA(float);
+    
 
 
 };
