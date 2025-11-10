e89144ad9dafab31f246744cb5c4739b37cca559
Julio Sánchez <106382109+Yesteb@users.noreply.github.com>
2025-11-10 11:19:50 -0500

    Add numeroCelular attribute and its accessors

--- src/model/Usuario.h
+++ src/model/Usuario.h
@@ -7,13 +7,24 @@ class Usuario {
 private:
     std::string nombre;
     std::string cedula;
+    std::string numeroCelular;
     std::string fechaNacimiento;
     
 
 public:
-    
+    Usuario();
+    ~Usuario();
 
+    //Getters
+    std::string getNombre();
+    std::string getCedula();
+    std::string getNumeroCelular();
+    std::string getFechaNacimiento();
 
-
+    //Setters
+    void setNombre(std::string);
+    void setCedula(std::string);
+    void setNumeroCelular(std::string);
+    void setFechaNacimiento(std::string);
 
 };
