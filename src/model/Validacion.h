495bfd9b964bc22667be2da3725e25da81d42d75
IvanYacelga <iayacelga@espe.edu.ec>
2025-11-10 12:05:27 -0500

    Comment out validarCantidad method in Validador class
    Comment out the validarCantidad method declaration.

--- src/model/Validacion.h
+++ src/model/Validacion.h
@@ -6,5 +6,5 @@ class Validador {
 public:
     static void validar(const std::string& texto, const std::string& tipo);
     static bool validarCedulaEcuatoriana(const std::string& cedula);
-    static std::string validarCantidad();
+   // static std::string validarCantidad();
 };
