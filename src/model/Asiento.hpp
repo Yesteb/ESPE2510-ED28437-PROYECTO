602ab922bd6f00bb70ee811b578d79bac51efb58
Julio Sánchez <106382109+Yesteb@users.noreply.github.com>
2025-11-10 10:24:23 -0500

    Implement Asiento class methods and properties
    Added constructor, destructor, getters, and setters for Asiento class.

--- src/model/Asiento.hpp
+++ src/model/Asiento.hpp
@@ -1,3 +1,15 @@
 #pragma once
 #include "Asiento.h"
 
+Asiento::Asiento(){}
+Asiento::~Asiento(){}
+
+//Getters
+char[2] Asiento::getNombreAsiento(){ return nombreAsiento; }
+std::string Asiento::getTipoAsiento(){ return tipoAsiento; }
+Usuario Asiento::getUsuario(){ return usuario; }
+
+//Setters
+void Asiento::setNombreAsiento(char[2] nombre) : nombreAsiento(nombre){}
+void Asiento::setTipoAsiento(std::string asiento) : tipoAsiento(asiento){}
+void Asiento::setUsuario(Usuario usuario) : duenioAsiento(usuario){}
