f8d401bef0ace8424252f356543bc6d024aeab3e
Julio Sánchez <106382109+Yesteb@users.noreply.github.com>
2025-11-10 10:14:24 -0500

    Enhance Asiento class with new members and methods
    Added constructor, destructor, getters, and setters for Asiento class.

--- src/model/Asiento.h
+++ src/model/Asiento.h
@@ -4,8 +4,22 @@
 
 class Asiento {
 private:
+    char[2] nombreAsiento;
     std::string tipoAsiento;
     Usuario usuario;
+public: 
+    Asiento();
+    ~Asiento();
     
+    //Getters
+    char getNombreAsiento();
+    std::string getTipoAsiento();
+    Usuario getUsuario();
+
+    //Setters
+    void setNombreAsiento(char[2]);
+    void setTipoAsiento(std::string);
+    void setUsuario(Usuario);
 
 }
+
