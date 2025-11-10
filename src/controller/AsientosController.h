9b5e32c502f5290614563d1e0041e521899c4eae
Julio Sánchez <106382109+Yesteb@users.noreply.github.com>
2025-11-10 12:15:14 -0500

    Add AsientosController class with reservation methods

--- src/controller/AsientosController.h
+++ src/controller/AsientosController.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <iostream>
+#include "Asiento.h"
+#include "FechaHora.h"
+#include "ListaDobleCircular.h"
+#include "Usuario.h"
+#include "Validacion.h"
+#include <string>
+
+class AsientosController{
+  public:
+    AsientosController();
+    ~AsientosController();
+
+
+    bool reservar(Asiento asiento);
+    bool eliminarReserva(Asiento asiento);
+    void buscarAsiento(std::string cedula);
+    void mostrarAsientoReservado(std::string cedula);
+};
+
+#include "AsientosController.hpp"
