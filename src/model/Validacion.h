24703a75ee7800b975a63435f17a51feaf527594
IvanYacelga <iayacelga@espe.edu.ec>
2025-11-11 15:39:04 -0500

    Add validarFecha method to Validacion class

--- src/model/Validacion.h
+++ src/model/Validacion.h
@@ -8,4 +8,5 @@ public:
     static bool validar(const std::string& texto, const std::string& tipo); 
     static bool validarCedulaEcuatoriana(const std::string& cedula);
     static std::string validarCantidad();
+    static bool validarFecha(const std::string& fecha); 
 };
